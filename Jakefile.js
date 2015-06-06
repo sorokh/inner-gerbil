3950d7a071ff2813b48087c047afe3d258ce56ac
frankdhondt <f.d.hondt@telenet.be>
2015-06-06 19:51:03 +0200

    Setup Jakefile for creating database

---
+++ Jakefile.js
@@ -0,0 +1,18 @@
+﻿var os = require('os');
+
+desc('Recreates database.');
+task('create-database', { async: true }, function () {
+    var cmds = [
+        'psql --dbname=lets -U postgres --file ./sql/schema.sql',
+        'psql --dbname=lets -U postgres --file ./sql/testdata.sql',
+        'psql --dbname=lets -U postgres --file ./sql/privileges.sql'
+    ];
+    jake.exec(cmds, { printStdout: true }, function () {
+        console.log('Database created.');
+        complete();
+    });
+    
+    // Test to log OS version if we need to do something different for windows
+    console.log(os.platform()); // 'darwin'
+    console.log(os.release()); //'10.8.0'
+})
