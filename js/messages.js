132654ee3d1b1af873b76a08884209b7b279c9ad
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2015-06-23 17:48:51 +0200

    moved utility function to sri4node

--- js/messages.js
+++ js/messages.js
@@ -6,47 +6,6 @@ exports = module.exports = function(sri4node) {
     var $s = sri4node.schemaUtils;
     var $q = sri4node.queryUtils;
 
-    /*
-        Add references from a different resource to this resource.
-        * type : the resource type that has a reference to the retrieved elements.
-        * column : the database column that contains the foreign key.
-        * key : the name of the key to add to the retrieved elements.
-    */
-    function addReferencingResources(type, column, targetkey) {
-        return function(database, elements) {
-            var deferred = Q.defer()
-
-            var tablename = type.split('/')[1]
-            var query = $u.prepareSQL()
-            var elementKeys = []
-            var elementKeysToElement = {}
-            elements.forEach(function(element) { 
-                var permalink = element.$$meta.permalink
-                var elementKey = permalink.split('/')[2]
-                elementKeys.push(elementKey)
-                elementKeysToElement[elementKey] = element
-            });
-            console.log(elements)
-            console.log(elementKeys)
-            query.sql('select key,' + column + ' as fkey from ' + tablename + ' where ' + column + ' in (').array(elementKeys).sql(')')
-            $u.executeSQL(database, query).then(function(result) {
-                result.rows.forEach(function(row) {
-                    var element = elementKeysToElement[row.fkey]
-                    if(!element[targetkey]) {
-                        element[targetkey] = []
-                    }
-                    element[targetkey].push(type + '/' + row.key)
-                });
-                deferred.resolve()
-            }).fail(function(e) {
-                console.log(e.stack)
-                deferred.reject()
-            })
-
-            return deferred.promise
-        }
-    }
-    
     return {
         type: "/messages",
         public: true,
@@ -101,9 +60,9 @@ exports = module.exports = function(sri4node) {
         query: {
         },
         afterread: [
-            addReferencingResources('/messagecontactdetails','message','$$contactdetails'),
-            addReferencingResources('/messageparties','message','$$parties'),
-            addReferencingResources('/messagetransactions','message','$$transaction')
+            $u.addReferencingResources('/messagecontactdetails','message','$$contactdetails'),
+            $u.addReferencingResources('/messageparties','message','$$parties'),
+            $u.addReferencingResources('/messagetransactions','message','$$transaction')
         ],
         afterupdate: [],
         afterinsert: [],
