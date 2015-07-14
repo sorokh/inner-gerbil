4f79f79f87cbcac86ac2a818f54bf4453050d97c
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2015-07-14 22:49:43 +0200

    added ?relatedToMessages to /contactdetails

--- js/contactdetails.js
+++ js/contactdetails.js
@@ -1,63 +1,98 @@
-/*jslint node: true */
-"use strict";
-
 exports = module.exports = function (sri4node) {
-    var $u = sri4node.utils,
-        $m = sri4node.mapUtils,
-        $s = sri4node.schemaUtils,
-        $q = sri4node.queryUtils;
-    
-    return {
-        type: "/contactdetails",
-        "public": true,
-        secure : [],
-        schema: {
-            $schema: "http://json-schema.org/schema#",
-            title: "A contact detail of one of the parties involves in a mutual credit system. It can be an adres, e-mail, website, facebook, etc.. etc..",
-            type: "object",
-            properties : {
-                type: {
-                    type: "string",
-                    description: "The type of contactdetail.",
-                    "enum": ["address", "email", "facebook", "website"]
-                },
-                label: $s.string("A display label for this contact detail."),
+  'use strict';
+  var $u = sri4node.utils,
+    $m = sri4node.mapUtils,
+    $s = sri4node.schemaUtils;
 
-                /* Generic value of the contact detail */
-                value: $s.string("Value for this contact detail. Addresses use different fields."),
-                /* Address fields */
-                street: $s.string("Streetname of the address of residence."),
-                streetnumber: $s.string("Street number of the address of residence."),
-                streetbus: $s.string("Postal box of the address of residence."),
-                zipcode: $s.belgianzipcode("4 digit postal code of the city for the address of residence."),
-                city: $s.string("City for the address of residence."),
-                latitude: $s.numeric("Latitude of the address."),
-                longitude: $s.numeric("Longitude of the address."),
+  function relatedToMessages(value, select) {
+    var q = $u.prepareSQL();
 
-                "public": $s.boolean("Is this contact detail visible to other members of your group (and all it's subgroups ?")
-            },
-            required: ["type", "public"]
+    var links, keys, key;
+    keys = [];
+    links = value.split(',');
+    links.forEach(function (link) {
+      key = link.split('/')[2];
+      keys.push(key);
+    });
+
+    q.sql('select contactdetail from messagecontactdetails where message in (')
+      .array(keys).sql(')');
+    select.with(q, 'relatedcontactdetails');
+    select.sql(' and key in (select contactdetail from relatedcontactdetails) ');
+  }
+
+  return {
+    type: '/contactdetails',
+    'public': true, // eslint-disable-line
+    secure: [],
+    schema: {
+      $schema: 'http://json-schema.org/schema#',
+      title: 'A contact detail of one of the parties involves in a mutual credit system. ' +
+        'It can be an adres, e-mail, website, facebook, etc.. etc..',
+      type: 'object',
+      properties: {
+        type: {
+          type: 'string',
+          description: 'The type of contactdetail.',
+          'enum': ['address', 'email', 'facebook', 'website']
         },
-        map: {
-            type: {},
-            label: { onread: $m.removeifnull },
-            value: { onread: $m.removeifnull },
+        label: $s.string('A display label for this contact detail.'),
 
-            street: { onread: $m.removeifnull },
-            streetnumber: { onread: $m.removeifnull },
-            streetbus: { onread: $m.removeifnull },
-            zipcode: { onread: $m.removeifnull },
-            city: { onread: $m.removeifnull },
-            latitude: { onread: $m.removeifnull },
-            longitude: { onread: $m.removeifnull },
+        /* Generic value of the contact detail */
+        value: $s.string('Value for this contact detail. Addresses use different fields.'),
+        /* Address fields */
+        street: $s.string('Streetname of the address of residence.'),
+        streetnumber: $s.string('Street number of the address of residence.'),
+        streetbus: $s.string('Postal box of the address of residence.'),
+        zipcode: $s.belgianzipcode('4 digit postal code of the city for the address of residence.'),
+        city: $s.string('City for the address of residence.'),
+        latitude: $s.numeric('Latitude of the address.'),
+        longitude: $s.numeric('Longitude of the address.'),
 
-            "public": {}
-        },
-        validate: [],
-        query: {
-        },
-        afterupdate: [],
-        afterinsert: [],
-        afterdelete: []
-    };
+        'public': // eslint-disable-line
+          $s.boolean('Is this contact detail visible to other members of your group (and all it\'s subgroups ?')
+      },
+      required: ['type', 'public']
+    },
+    map: {
+      type: {},
+      label: {
+        onread: $m.removeifnull
+      },
+      value: {
+        onread: $m.removeifnull
+      },
+
+      street: {
+        onread: $m.removeifnull
+      },
+      streetnumber: {
+        onread: $m.removeifnull
+      },
+      streetbus: {
+        onread: $m.removeifnull
+      },
+      zipcode: {
+        onread: $m.removeifnull
+      },
+      city: {
+        onread: $m.removeifnull
+      },
+      latitude: {
+        onread: $m.removeifnull
+      },
+      longitude: {
+        onread: $m.removeifnull
+      },
+
+      'public': {} // eslint-disable-line
+    },
+    validate: [],
+    query: {
+      relatedToMessages: relatedToMessages
+    },
+    afterupdate: [],
+    afterinsert: [],
+    afterdelete: []
+  };
 };
