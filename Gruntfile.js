6ab8493c9d6cd97f403b3d9c2f1ed907817c5a27
frankdhondt <f.d.hondt@telenet.be>
2015-09-20 19:51:21 +0200

    Added tests as target for eslint in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
         config: '.eslintrc',
         reset: true
       },
-      target: ['js/**/*.js', 'Jakefile.js', 'Gruntfile.js']
+      target: ['js/**/*.js', 'test/**/*.js', 'Jakefile.js', 'Gruntfile.js']
     }
   });
 
