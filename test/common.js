ea0947a0af651ee6e2f1ace252bcf33b719aebc2
Bernard Butaye <bernardenmartine@gmail.com>
2015-10-10 21:21:06 +0200

    add test on insert and update party

--- test/common.js
+++ test/common.js
@@ -1,4 +1,3 @@
-
 exports = module.exports = {
   createHrefArray: function (response) {
     'use strict';
@@ -22,6 +21,17 @@ exports = module.exports = {
   cl: function (x) {
     'use strict';
     console.log(x); // eslint-disable-line
+  },
+
+  generateUUID: function () {
+    'use strict';
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
   },
 
   hrefs: {
