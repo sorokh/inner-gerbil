d0eedc96cdd24f28eab9c8a2ceff03e03143e372
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2015-10-11 17:13:17 +0000

    added uuid in package.json, used it in common.generateUUID - linting of source is now ok.

--- test/common.js
+++ test/common.js
@@ -1,3 +1,5 @@
+var uuid = require('uuid');
+
 exports = module.exports = {
   createHrefArray: function (response) {
     'use strict';
@@ -25,13 +27,7 @@ exports = module.exports = {
 
   generateUUID: function () {
     'use strict';
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = (d + Math.random() * 16) % 16 | 0;
-      d = Math.floor(d / 16);
-      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-    return uuid;
+    return uuid.v4();
   },
 
   hrefs: {
