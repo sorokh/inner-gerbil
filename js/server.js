3b04ca6722ec8f212dc0bdda52c09f536799c48c
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2015-11-22 13:36:06 +0000

    temp. disabled import test, added CORS support

--- js/server.js
+++ js/server.js
@@ -16,6 +16,40 @@ if (c9hostname) {
   cl('https://' + c9hostname);
 }
 
+var typeToConfig = function (config) {
+  'use strict';
+  var i;
+  var ret = {};
+  for (i = 0; i < config.length; i++) {
+    ret[config[i].type] = config[i];
+  }
+  return ret;
+};
+
+function allowCrossDomain(req, res, next) {
+  'use strict';
+  var allowedMethods = 'GET,PUT,POST,DELETE,HEAD,OPTIONS';
+
+  var origin = '*';
+  if (req.headers.origin) {
+    origin = req.headers.origin;
+  } else if (req.headers.Origin) {
+    origin = req.headers.Origin;
+  }
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Methods', allowedMethods);
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.header('Allow', allowedMethods);
+    res.status(200).send(allowedMethods);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 app.use(compress());
 app.set('port', process.env.PORT || 5000); // eslint-disable-line
 sri4node.configure(app, pg, mapping);
